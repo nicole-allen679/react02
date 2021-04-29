f4a018a8a53563b60cbf7b89d6299621f9f7889d
nicole-allen679 <nicole.allen679@gmail.com>
2021-04-29 14:43:27 -0400

    created a counter using state

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Main() {
-    return <p>Main</p>
+  const [counter, setCounter] = useState(0)
+  return (
+    <>
+      <p>The count is {counter}.</p>
+      <button onClick={() => setCounter(counter + 1)}>+1</button>
+      <button onClick={() => setCounter(0)}>reset</button>
+      <button onClick={() => setCounter(Math.max(0, counter - 1))}>-1</button>
+    </>
+  )
 }
 
 export default Main
